f114d83aed130bb6d81feac4213fb2afa861e162
tooBusyNow <OldsmobileBuick54@yandex.ru>
2021-04-25 19:11:45 +0500

    Added path

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 ## List of changes and dates:
     21.04: 
           - Added an ability to **place objects** and **visualization of "placeability"**
-    25.04:
+    24.04:
           - Useless scripts **were removed**. Improved code purity and safety
           - Fixed an incorrect spawn bug (Well, it looks like fixed, but who knows)
+    25.04:
+          - Added **path** for the enemies
    
--- TowerDefence/Assets/Scripts/SpanGridScript.cs
+++ TowerDefence/Assets/Scripts/SpanGridScript.cs
@@ -14,6 +14,11 @@ public class SpanGridScript : MonoBehaviour
 
     private Vector3 GridZeroPoint = Vector3.zero;
     private float GridSpacingOffset = 5;
+    private HashSet<int> hashSet = new HashSet<int>()
+    { 0, 1, 17, 18, 19, 20, 21, 37, 53, 69, 85, 101, 117, 133, 134,
+    135, 136, 137, 138, 154, 170, 186, 202, 201, 217, 233, 234, 235,
+    236, 237, 238, 239, 255};
+
 
     void Start()
     {
@@ -21,18 +26,21 @@ public class SpanGridScript : MonoBehaviour
     }
     void SpawnGrid()
     {
+        int index = 0;
         for (int x = 0; x < GridX; x++)
         { 
             for (int y = 0; y < GridY; y++)
-            {
+            {      
                 var spawnPosition = new Vector3 (x * GridSpacingOffset, 0, y * GridSpacingOffset) + GridZeroPoint;
-                PickAndSpawn(spawnPosition, Quaternion.identity);
+                PickAndSpawn(spawnPosition, Quaternion.identity, index);
+                index++;
             }
         }
     }
-    void PickAndSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
+    void PickAndSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn, int index)
     {
-        var randomIndex = Random.Range(0, itemsToPickFrom.Length);
-        var clone = Instantiate(itemsToPickFrom[randomIndex], positionToSpawn, rotationToSpawn);
+        int id = 0;
+        if (hashSet.Contains(index)) id = 1;
+        var clone = Instantiate(itemsToPickFrom[id], positionToSpawn, rotationToSpawn);
     }
 }        
