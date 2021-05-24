3c2a28724a2e8f3ef069bd40af34fc28d4c6f486
Daniil <49916703+sicutglacies@users.noreply.github.com>
2021-05-24 21:43:24 +0500

    shocked

--- TowerDefence/Assets/Scripts/EnemyScript.cs
+++ TowerDefence/Assets/Scripts/EnemyScript.cs
@@ -7,11 +7,7 @@ public class EnemyScript : MonoBehaviour
     public float speed = 10f;
     private Transform target;
     private int wayPointerIndex;
-<<<<<<< HEAD
-    private List<GameObject> wayPointers;
-=======
     private GameObject[] wayPointers;
->>>>>>> origin/PrefabWorks
 
     // Start is called before the first frame update
     void Start()
@@ -23,17 +19,10 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        Vector3 dir = target.position - transform.position + new Vector3(0, 5, 0);
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(transform.position, target.position + new Vector3(0, 5, 0)) <= 0.2f)
-=======
         Vector3 dir = target.position - transform.position + new Vector3(0, 2, 0);
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
         if (Vector3.Distance(transform.position, target.position + new Vector3(0, 2, 0)) <= 0.2f)
->>>>>>> origin/PrefabWorks
         {
             GetNextWayPointer();
         }
@@ -41,12 +30,8 @@ public class EnemyScript : MonoBehaviour
 
     void GetNextWayPointer()
     {
-<<<<<<< HEAD
-        if (wayPointerIndex >= wayPointers.Count - 1)
-=======
         Debug.Log("now here");
         if (wayPointerIndex >= wayPointers.Length - 1)
->>>>>>> origin/PrefabWorks
         {
             Destroy(gameObject);
             return;
@@ -55,8 +40,4 @@ public class EnemyScript : MonoBehaviour
         wayPointerIndex++;
         target = wayPointers[wayPointerIndex].transform;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/PrefabWorks
