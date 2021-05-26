c0300be5e5deca7139291f45eba7ccde4f85c0b0
P11astic <4e1oweknopauk@gmail.com>
2021-05-26 20:13:40 +0500

    DungeonMaster

--- TowerDefence/Assets/Scripts/EnemyScript.cs
+++ TowerDefence/Assets/Scripts/EnemyScript.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public float speed = 10f;
-
     private Transform target;
     private int wayPointerIndex;
     private GameObject[] wayPointers;
 
+    // Start is called before the first frame update
     void Start()
     {
         wayPointers = GameObject.Find("GridSpawner").GetComponent<SpanGridScript>().wayPointers;
         target = wayPointers[0].transform;
     }
 
+    // Update is called once per frame
     void Update()
     {
         Vector3 dir = target.position - transform.position + new Vector3(0, 2, 0);
@@ -29,6 +30,7 @@ public class EnemyScript : MonoBehaviour
 
     void GetNextWayPointer()
     {
+        //Debug.Log("now here");
         if (wayPointerIndex >= wayPointers.Length - 1)
         {
             Destroy(gameObject);
